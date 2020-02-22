a61083e562ca34628774edf0ce324fa0bb87f28f
alecapua <alecapua@icloud.com>
2020-02-21 21:26:32 -0500

    E06a-Particles

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -2,32 +2,43 @@ extends Camera2D
 
 export var decay_rate = 0.4
 export var max_offset = 20
+export var acceleration = 1
  
 var _start_position
 var _start_rotation
 var _trauma
+var velocity = Vector2(0,0)
  
 func _ready():
-	_start_position = position
-	_trauma = 0.0
+_start_position = position
+_trauma = 0.0
 
 func add_trauma(amount):
-	_trauma = min(_trauma + amount, 1)
+_trauma = min(_trauma + amount, 10)
+if Input. is_action_pressed("Left"):
+velocity.x -= acceleration
+if Input. is_action_pressed("Right"):
+velocity.x += acceleration
+if Input. is_action_pressed("Up"):
+velocity.y -= acceleration
+if Input. is_action_pressed("Down"):
+velocity.y += acceleration
+
  
 func _process(delta):   
-	if _trauma > 0:
-		_decay_trauma(delta)
-		_apply_shake()
-	   
+if _trauma > 0:
+_decay_trauma(delta)
+_apply_shake()
+  
 func _decay_trauma(delta):
-	var change = decay_rate * delta
-	_trauma = max(_trauma - change, 0)
+var change = decay_rate * delta
+_trauma = max(_trauma - change, 0)
  
 func _apply_shake():
-	var shake = _trauma * _trauma
-	var o_x = max_offset * shake * _get_neg_or_pos_scalar()
-	var o_y = max_offset * shake * _get_neg_or_pos_scalar()
-	position = _start_position + Vector2(o_x, o_y)
+var shake = _trauma * _trauma
+var o_x = max_offset * shake * _get_neg_or_pos_scalar()
+var o_y = max_offset * shake * _get_neg_or_pos_scalar()
+position = _start_position + Vector2(o_x, o_y)
  
 func _get_neg_or_pos_scalar():
-	return rand_range(-1.0, 1.0)
+return rand_range(-1.0, 1.0)
